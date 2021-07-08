f40aa79beedb9c9289db3535fbc44c7d60e1ede6
RJR <80534043+rjr2@users.noreply.github.com>
2021-07-08 01:26:39 -0400

    HTML and JS update

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+//Search variables
+var searchBtn = document.getElementById("btn");
+var input = document.getElementById("input");
+//Current temp display variables
+var currentTemp = document.getElementById("temp");
+var currentWind = document.getElementById("wind");
+var currentHumid = document.getElementById("humidity");
+var currentUv = document.getElementById("uvindex");
+//Five day forecast variables
+var dayOne = document.getElementById("day1");
+var dayTwo = document.getElementById("day2");
+var dayThree = document.getElementById("day3");
+var dayFour = document.getElementById("day4");
+var dayFive = document.getElementById("day5");
+//Previous search Variables
+var cityOne = document.getElementById("city1");
+var cityTwo = document.getElementById("city2");
+var cityThree = document.getElementById("city3");
+var cityFour = document.getElementById("city4");
+var cityFive = document.getElementById("city5");
+
